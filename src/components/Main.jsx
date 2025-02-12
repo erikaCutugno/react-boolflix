71dc29df047b36ba81d276c381f34b87942c0430
erikaCutugno <erikucciapop@gmail.com>
2025-02-12 19:58:07 +0100

    feat: add star

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,23 @@
 import { useArticleContext } from "../contexts/ArticleContext";
 import flags from "../data/flag";
+import Stars from "./Stars";
+
 export default function Main() {
   const { movies, series } = useArticleContext();
+
+  // converto i voti in stelle
+  const voteTot = (film) => {
+    const vote = Math.round((film.vote_average - 1) / 2 + 1);
+    // let star = [];
+    // for (let i = 1; i <= 5; i++) {
+    //   if (vote >= i) {
+    //     star.push(<i className="fa-solid fa-star"></i>);
+    //   } else {
+    //     star.push(<i className="fa-regular fa-star"></i>);
+    //   }
+    // }
+    return vote;
+  };
 
   return (
     <main>
@@ -9,31 +25,32 @@ export default function Main() {
       <ul>
         {movies.map((movie) => (
           <li key={movie.id}>
+            <div>Titolo:{movie.title}</div>
+            <div>Titolo Originale:{movie.original_title}</div>
             <div>
-              <div>Titolo:{movie.title}</div>
-              <div>Titolo Originale:{movie.original_title}</div>
-              <div>
-                Lingua:
-                {movie.original_language == flags.language ? (
-                  <img
-                    className="flag"
-                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${flags.country}.svg`}
-                    alt={movie.original_language}
-                  />
-                ) : (
-                  <img
-                    className="flag"
-                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${movie.original_language.toUpperCase()}.svg`}
-                    alt={movie.original_language}
-                  />
-                )}
-              </div>
-              <div>Voto:{movie.vote_average}</div>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                alt=""
-              />
+              Lingua:
+              {movie.original_language == flags.language ? (
+                <img
+                  className="flag"
+                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${flags.country}.svg`}
+                  alt={movie.original_language}
+                />
+              ) : (
+                <img
+                  className="flag"
+                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${movie.original_language.toUpperCase()}.svg`}
+                  alt={movie.original_language}
+                />
+              )}
             </div>
+            <div>
+              Voto:
+              <Stars vote={voteTot(movie)} />
+            </div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+              alt={movie.title}
+            />
           </li>
         ))}
       </ul>
@@ -41,31 +58,32 @@ export default function Main() {
       <ul>
         {series.map((serie) => (
           <li key={serie.id}>
+            <div>Titolo:{serie.name}</div>
+            <div>Titolo Originale:{serie.original_name}</div>
             <div>
-              <div>Titolo:{serie.name}</div>
-              <div>Titolo Originale:{serie.original_name}</div>
-              <div>
-                Lingua:
-                {serie.original_language == flags.language ? (
-                  <img
-                    className="flag"
-                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${flags.country}.svg`}
-                    alt={serie.original_language}
-                  />
-                ) : (
-                  <img
-                    className="flag"
-                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${serie.original_language.toUpperCase()}.svg`}
-                    alt={serie.original_language}
-                  />
-                )}
-              </div>
-              <div>Voto:{serie.vote_average}</div>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
-                alt=""
-              />
+              Lingua:
+              {serie.original_language == flags.language ? (
+                <img
+                  className="flag"
+                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${flags.country}.svg`}
+                  alt={serie.original_language}
+                />
+              ) : (
+                <img
+                  className="flag"
+                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${serie.original_language.toUpperCase()}.svg`}
+                  alt={serie.original_language}
+                />
+              )}
             </div>
+            <div>
+              Voto:
+              <Stars vote={voteTot(serie)} />
+            </div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
+              alt={series.title}
+            />
           </li>
         ))}
       </ul>
