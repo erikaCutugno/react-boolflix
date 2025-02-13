b6cd08382e7f7598e5a7c7c9c96c871df635a7eb
erikaCutugno <erikucciapop@gmail.com>
2025-02-13 10:49:17 +0100

    feat: add css header and card movies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import SearchBar from "./SearchBar";
 export default function Header() {
   return (
     <header>
-      <h1>BoolFlix</h1>
-      <SearchBar />
+      <div className="row">
+        <h1>BoolFlix</h1>
+        <SearchBar />
+      </div>
     </header>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,86 +22,92 @@ export default function Main() {
 
   return (
     <main>
-      <h2>Lista Film</h2>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <div>Titolo:{movie.title}</div>
-            <div>Titolo Originale:{movie.original_title}</div>
-            <div>
-              Lingua:
-              {/* <LanguageFlag movie={movie.original_language} /> */}
-              {flags.find(
-                (flag) => movie.original_language == flag.language
-              ) ? (
+      <div className="container">
+        <h2>Lista Film</h2>
+        <ul className="row">
+          {movies.map((movie) => (
+            <li key={movie.id} className="card">
+              <div className="hover-card">
+                <div>Titolo:{movie.title}</div>
+                <div>Titolo Originale:{movie.original_title}</div>
+                <div>
+                  Lingua:
+                  {/* <LanguageFlag movie={movie.original_language} /> */}
+                  {flags.find(
+                    (flag) => movie.original_language == flag.language
+                  ) ? (
+                    <img
+                      className="flag"
+                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${
+                        flags.find(
+                          (flag) => movie.original_language == flag.language
+                        ).country
+                      }.svg`}
+                      alt={movie.original_language}
+                    />
+                  ) : (
+                    <img
+                      className="flag"
+                      src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${movie.original_language.toUpperCase()}.svg`}
+                      alt={movie.original_language}
+                    />
+                  )}
+                </div>
+                <div>
+                  Voto:
+                  <Stars vote={voteTot(movie)} />
+                </div>
+              </div>
+              <div className="poster">
                 <img
-                  className="flag"
-                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${
-                    flags.find(
-                      (flag) => movie.original_language == flag.language
-                    ).country
-                  }.svg`}
-                  alt={movie.original_language}
+                  src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+                  alt={movie.title}
                 />
-              ) : (
-                <img
-                  className="flag"
-                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${movie.original_language.toUpperCase()}.svg`}
-                  alt={movie.original_language}
-                />
-              )}
-            </div>
-            <div>
-              Voto:
-              <Stars vote={voteTot(movie)} />
-            </div>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-              alt={movie.title}
-            />
-          </li>
-        ))}
-      </ul>
-      <h2>Lista Serie Tv</h2>
-      <ul>
-        {series.map((serie) => (
-          <li key={serie.id}>
-            <div>Titolo:{serie.name}</div>
-            <div>Titolo Originale:{serie.original_name}</div>
-            <div>
-              Lingua:
-              {/* <LanguageFlag movie={serie.original_language} /> */}
-              {flags.find(
-                (flag) => serie.original_language == flag.language
-              ) ? (
-                <img
-                  className="flag"
-                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${
-                    flags.find(
-                      (flag) => serie.original_language == flag.language
-                    ).country
-                  }.svg`}
-                  alt={serie.original_language}
-                />
-              ) : (
-                <img
-                  className="flag"
-                  src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${serie.original_language.toUpperCase()}.svg`}
-                  alt={serie.original_language}
-                />
-              )}
-            </div>
-            <div>
-              Voto:
-              <Stars vote={voteTot(serie)} />
-            </div>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
-              alt={series.title}
-            />
-          </li>
-        ))}
-      </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <h2>Lista Serie Tv</h2>
+        <ul>
+          {series.map((serie) => (
+            <li key={serie.id}>
+              <div>Titolo:{serie.name}</div>
+              <div>Titolo Originale:{serie.original_name}</div>
+              <div>
+                Lingua:
+                {/* <LanguageFlag movie={serie.original_language} /> */}
+                {flags.find(
+                  (flag) => serie.original_language == flag.language
+                ) ? (
+                  <img
+                    className="flag"
+                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${
+                      flags.find(
+                        (flag) => serie.original_language == flag.language
+                      ).country
+                    }.svg`}
+                    alt={serie.original_language}
+                  />
+                ) : (
+                  <img
+                    className="flag"
+                    src={`https://purecatamphetamine.github.io/country-flag-icons/3x2/${serie.original_language.toUpperCase()}.svg`}
+                    alt={serie.original_language}
+                  />
+                )}
+              </div>
+              <div>
+                Voto:
+                <Stars vote={voteTot(serie)} />
+              </div>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${serie.backdrop_path}`}
+                alt={series.title}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
